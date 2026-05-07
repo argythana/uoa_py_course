79a284d43cfccc42f63c70ff1572c6e794d912ae
Thanasis Argyriou <argythana@gmail.com>
2026-05-07 23:17:57 +0300

    Lecture 03: split into reading_material/ and practice_exercises/
    Move existing materials into the new subfolder layout and add practice_lec_03.py (lec 3 portion of the previously combined practice_lectures_2_3.py). Minor execution-state updates to instruct_03c_pip_venv.ipynb and instruct_03d_jupyterlab.ipynb.

---
+++ lectures_01_06_fundamentals_for_data_science/lecture_03_cwd_path_pip_venv_imports/practice_exercises/practice_lec_03.py
@@ -0,0 +1,53 @@
+## Practice Exercises for Lecture 03: CWD, Path, pip, venv, Imports
+## After reading lecture 03 material, solve the exercises below.
+
+'''
+These exercises require importing modules.
+Use help(module) and help(module.method) to explore available functions.
+'''
+
+
+# Exercise 01: Find the maximum, minimum and max-min range of the following sample of numbers:
+# sample = [170, 155, 165, 185, 180, 160, 175, 170, 170]
+'''Hint: Do Not Repeat Yourself.'''
+
+
+# Exercise 02: Find the mean, variance and standard deviation of the sample above
+'''
+Hint: import statistics module and use help(statistics) to find the methods.
+Use help(module.method) (substitute the respective names)
+'''
+
+
+# Exercise 03: Find the square root of log(10) and the square of log(e)
+'''Hint: use math.log and math.sqrt'''
+
+
+# Exercise 04: Find the value of the standard logistic function.
+'''
+-> https://en.wikipedia.org/wiki/Logistic_function
+for x = 2 (k = 1, x0 = 0 and L = 1)
+Hint: use math.e
+'''
+
+
+# Exercise 05: Swap two variables values.
+'''
+Define two variables: A = 4 and B = 25.
+Then replace each other's value (swap values) such that:
+print(A, B) returns 25, 4.
+Do this without explicitly using numbers again.
+Hint: there are more than one solutions,
+e.g. use an extra variable, use math operations, ...
+'''
+
+
+# Exercise 06: Working with paths
+'''
+a) Import the os module.
+b) Print your current working directory using os.getcwd().
+c) List the files in your current directory using os.listdir().
+d) Use os.path.join() to create a file path that would work on any operating system.
+   Example: join your cwd with a folder name "data" and a file "output.txt".
+e) Check if a file or folder exists using os.path.exists().
+'''
